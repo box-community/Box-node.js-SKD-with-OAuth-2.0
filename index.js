b23fe1b87bc0b193dffeef4064d301914c68103a
Mateusz Chojecki <mat@MacBook-Pro-Mateusz.local>
2023-05-09 00:32:22 +0200

    refactor usage of vanilla node to express web server

--- index.js
+++ index.js
@@ -1,80 +1,89 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
 
 const BoxSDK = require('box-node-sdk');
+
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-var sdk = new BoxSDK({
+const sdk = new BoxSDK({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 });
 
-var authURL = sdk.getAuthorizeURL({
-  response_type: 'code'
+const authURL = sdk.getAuthorizeURL({
+  response_type: 'code',
+  redirect_uri: `http://localhost:${PORT}/authorize_callback`
 });
 
-const templateLogin = fs.readFileSync(
-    `${__dirname}/templates/index.html`,
-    'utf-8'
+const app = express();
+
+app.use(
+  // Session default in memory store not production ready. Will leak memory over time
+  // Use redis or some other MemoryStore impl.
+  session({
+    secret: '68747470733a2f2f747769747465722e636f6d2f6d617443686f6a65636b79',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true } // secure will set cookies only on https but we in localhost here
+  })
 );
 
-const templateAccount = fs.readFileSync(
-    `${__dirname}/templates/account.html`,
-    'utf-8'
-);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'templates'));
 
-const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
-    // TODO if session exists redirect to account
-    if (pathname === '/' || pathname === '/login') {
-    res.writeHead(200, {
-      'Content-type': 'text/html'
-    });
+// All restricted pages should use this middleware
+// Well tbh honest most secure would be to use app.use(auth_middleware) for all app and exclude public pages
+// But this way is easier :D
+const auth_middleware = (req, res, next) => {
+  if (req.session.user) return next();
 
-    const output = templateLogin.replace('{%LOGIN_HREF%}', authURL);
-    res.end(output);
-  } else if (pathname === '/account') {
-    const code = query.code;
-    if (code) {
-        sdk.getTokensAuthorizationCodeGrant(code, null, function(err, tokenInfo) {
-            let client = sdk.getPersistentClient(tokenInfo);
+  res.redirect('/login');
+};
 
-            res.writeHead(200, {
-              'Content-type': 'text/html'
-            });
+app.get('/', auth_middleware, (req, res) => {
+  const user = req.session.user;
 
-            let name = ''
-            let output = templateAccount
-            const user = client.users.get(client.CURRENT_USER_ID);
-            user.then((val => {
-              name = val.name
-              id = val.id;
-              output = templateAccount.replace('{%USER_NAME%}', name).replace('{%USER_ID%}', id);
-              res.end(output);
-            })).catch((err) => {
-              console.log(err)
-              res.end('<h1>Could not fetch data</h1>');
-            })
-        });
-    } else {
-        res.writeHead(401, {
-            'Content-type': 'text/html',
-          });
-        res.end('<h1>Not autenticated!</h1>');
-    }
-  } else {
-    res.writeHead(404, {
-      'Content-type': 'text/html',
-    });
-    res.end('<h1>Page not found!</h1>');
-  }
+  res.render('index', {
+    name: user.name,
+    id: user.id
+  })
 });
 
-server.listen(PORT, '127.0.0.1', () => {
-    console.log(`Listening to requests on port http://localhost:${PORT}`);
+app.get('/login', (req, res) => {
+  res.render('login', { login_url: authURL });
 });
+
+
+app.get('/authorize_callback/:code', async (req, res) => {
+  const { code } = req.query;
+
+  if (!code) return res.status(401).send("Access denied");
+
+  const tokenInfo = await sdk.getTokensAuthorizationCodeGrant(code);
+  const client = sdk.getPersistentClient(tokenInfo);
+  const user = await client.users.get(client.CURRENT_USER_ID);
+
+  // Save user in session for persistence, this is basic of auth middleware implementation
+  req.session.user = user;
+  req.session.user_token = tokenInfo; // Split storing access token and refresh token for production
+
+  req.redirect('/account');
+});
+
+app.post('/logout', (req, req) => {
+  req.session.destroy(); // Removes session data from req object
+  res.redirect('/login');
+})
+
+// WIldcard for not defined URLs
+app.use('*', (req, res) => {
+  res.setHeader('Content-type', 'text/html')
+  res.status(404).header().end('<h1>Page not found!</h1>');
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening to requests on port http://localhost:${PORT}`);
+});
+
